873d8fc1cff91d8ca16fee8f66fb4bf1a7d1b255
rajveerbola <49003940+rajveerbola@users.noreply.github.com>
2021-04-10 22:05:05 -0700

    Update app.py

--- processing/app.py
+++ processing/app.py
@@ -92,8 +92,8 @@ def init_scheduler():
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-#CORS(app.app)
-#app.app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
