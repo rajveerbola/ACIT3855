ca5bb99432c10ef6028f8040490b9184a795e0a6
rajveerbola <49003940+rajveerbola@users.noreply.github.com>
2021-04-19 22:31:04 -0700

    Update AppStats.js

--- dashboard/src/components/AppStats.js
+++ dashboard/src/components/AppStats.js
@@ -8,7 +8,7 @@ export default function AppStats() {
 
 	const getStats = () => {
 	
-        fetch(`http://acit38555.eastus.cloudapp.azure.com:8100/stats`)
+        fetch(`http://acit38555.eastus.cloudapp.azure.com/processing/stats`)
             .then(res => res.json())
             .then((result)=>{
 				console.log("Received Stats")
