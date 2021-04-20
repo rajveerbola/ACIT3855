19f7946b1aa74f3a61e132659728bdb0a015b5fb
rajveerbola <49003940+rajveerbola@users.noreply.github.com>
2021-04-19 23:42:47 -0700

    Update app.py

--- receiver/app.py
+++ receiver/app.py
@@ -79,7 +79,7 @@ def cleaning_product_order(body):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml", base_path"/receiver", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", base_path"/receiver" , strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     app.run(port=8080)
