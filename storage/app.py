e9b16192255cd33b9c0e1ed4e08a4aea7f65dd46
rajveerbola <49003940+rajveerbola@users.noreply.github.com>
2021-04-21 19:09:03 -0700

    Update app.py

--- storage/app.py
+++ storage/app.py
@@ -111,10 +111,10 @@ def get_cleaning_product_order(start_timestamp, end_timestamp):
     start_timestamp_datetime = datetime.datetime.strptime(start_timestamp, "%Y-%m-%dT%H:%M:%S")
     end_timestamp_datetime = datetime.datetime.strptime(end_timestamp, "%Y-%m-%dT%H:%M:%S")      
     print(timestamp_datetime)
-
+    
     readings = session.query(CleaningProductOrder).filter(and_(CleaningProductOrder.date_created >=
-                                            start_timestamp_datetime,
-                                            CleaningProductOrder.date_created < end_timestamp_datetime))
+                                                        start_timestamp_datetime,
+                                                        CleaningProductOrder.date_created < end_timestamp_datetime))
     results_list = []
     print(readings)
     for reading in readings:
